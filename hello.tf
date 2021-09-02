d1b90d34d50d4cf3a3f007cd72973cc7c2831005
joy larkin <joylarkin@users.noreply.github.com>
2021-09-02 16:04:10 -0700

    Update hello.tf

--- hello.tf
+++ hello.tf
@@ -16,14 +16,14 @@ target = "192.168.42.0/24"
 
 resource "zerotier_member" "JOY" {
 name        = "joy"
-member_id   = "2d239ff869"
+member_id   = "xxx"
 description = "joy phone"
 network_id  = zerotier_network.hello.id
 }
 
 resource "zerotier_member" "jl" {
 name        = "jl"
-member_id   = "045a541f06"
-description = "joy's laptop"
+member_id   = "xxx"
+description = "jl"
 network_id  = zerotier_network.hello.id
 }
